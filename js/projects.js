d0e4930a3b5bca104d2b15009e0cb7b9cd8a1251
Abdollah Ebadi <a.ebadi@boundless-systems.com.au>
2024-04-20 15:20:05 +1000

    implement the tabs for the projects page

---
+++ js/projects.js
@@ -0,0 +1,39 @@
+//In the name of Allah
+
+let tabs = document.querySelectorAll(".tab")
+
+let tabIndicator = document.querySelector(".tab-indicator")
+
+tabIndicator.style.setProperty("--width", `${(tabIndicator.offsetWidth) / (tabs.length)}px`)
+
+let tabContents = document.querySelectorAll(".tab-content")
+
+tabContents[0].classList.add("show-tab")
+
+
+
+for(let i = 0 ; i < tabs.length ; i++) {
+    tabs[i].addEventListener("click" , function() {
+
+        // Section to manage the tab indicator animation
+        let actualLeft = (tabIndicator.offsetWidth) / (tabs.length) * i
+        tabIndicator.style.setProperty("--left", `${actualLeft}px`)
+        tabIndicator.style.setProperty("--width", `${(tabIndicator.offsetWidth) / (tabs.length)}px`)
+
+        // Section to switch the tab contents
+        showTabContent(i)
+
+    })
+}
+
+
+function showTabContent(currentTabIndex) {
+    tabContents[currentTabIndex].classList.add("show-tab")
+    for(let j = 0 ; j < tabContents.length ; j++) {
+        if( j !== currentTabIndex) {
+            tabContents[j].classList.remove("show-tab")
+        }
+    }
+}
+
+
